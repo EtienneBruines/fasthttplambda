1e56525cd98b475146f4e08251f17260dcdb9a57
Derek <dkhorton2002@gmail.com>
2023-11-16 14:53:24 -0500

    Add support for lambda functions & api gateway

--- lambda.go
+++ lambda.go
@@ -10,11 +10,32 @@ import (
 	"github.com/valyala/fasthttp/fasthttputil"
 )
 
-type ProxyOutput struct {
-	IsBase64Encoded bool              `json:"isBase64Encoded"`
+type Request struct {
+	// Lambda function url methods
+	Version        string                                 `json:"version"` // Version is expected to be `"2.0"`
+	RawPath        string                                 `json:"rawPath"`
+	RawQueryString string                                 `json:"rawQueryString"`
+	RequestContext events.LambdaFunctionURLRequestContext `json:"requestContext"`
+
+	// Api Gateway methods
+	Path       string `json:"path"` // The url path for the caller
+	HTTPMethod string `json:"httpMethod"`
+
+	// Shared methods
+
+	QueryStringParameters map[string]string `json:"queryStringParameters,omitempty"`
+	Cookies               []string          `json:"cookies,omitempty"`
+	Headers               map[string]string `json:"headers"`
+	Body                  string            `json:"body,omitempty"`
+	IsBase64Encoded       bool              `json:"isBase64Encoded"`
+}
+
+type Response struct {
 	StatusCode      int               `json:"statusCode"`
-	Body            string            `json:"body"`
 	Headers         map[string]string `json:"headers"`
+	Body            string            `json:"body"`
+	IsBase64Encoded bool              `json:"isBase64Encoded"`
+	Cookies         []string          `json:"cookies"`
 }
 
 func server(l net.Listener, handler fasthttp.RequestHandler) {
@@ -32,8 +53,8 @@ func server(l net.Listener, handler fasthttp.RequestHandler) {
 	}
 }
 
-func Handle(handler fasthttp.RequestHandler) func(event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return func(event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func Handle(handler fasthttp.RequestHandler) func(event Request) (Response, error) {
+	return func(event Request) (Response, error) {
 		//func Handle(event *apigatewayproxyevt.Event, ctx *runtime.Context) (*ProxyOutput, error) {
 		l := fasthttputil.NewInmemoryListener()
 		go server(l, handler)
@@ -44,30 +65,44 @@ func Handle(handler fasthttp.RequestHandler) func(event events.APIGatewayProxyRe
 		)
 
 		uri := url.URL{}
+
 		uri.Path = event.Path
+		if uri.Path == "" {
+			uri.Path = event.RequestContext.HTTP.Path
+		}
+
 		uri.Host = "localhost"
 
-		vals := url.Values{}
-		for k, v := range event.QueryStringParameters {
-			vals.Set(k, v)
+		uri.RawQuery = event.RawQueryString
+		if uri.RawQuery == "" {
+			vals := url.Values{}
+			for k, v := range event.QueryStringParameters {
+				vals.Set(k, v)
+			}
+			uri.RawQuery = vals.Encode()
 		}
-		uri.RawQuery = vals.Encode()
 
 		req.SetRequestURI(uri.RequestURI())
 		req.SetHost("localhost")
 		if event.IsBase64Encoded {
 			body, err := base64.StdEncoding.DecodeString(event.Body)
 			if err != nil {
-				return events.APIGatewayProxyResponse{}, err
+				return Response{}, err
 			}
 			req.SetBody(body)
 		} else {
 			req.SetBody([]byte(event.Body))
 		}
+
 		for k, v := range event.Headers {
 			req.Header.Add(k, v)
 		}
-		req.Header.SetMethod(event.HTTPMethod)
+
+		if event.HTTPMethod == "" {
+			req.Header.SetMethod(event.RequestContext.HTTP.Method)
+		} else {
+			req.Header.SetMethod(event.HTTPMethod)
+		}
 
 		client := fasthttp.Client{
 			Dial: func(string) (net.Conn, error) { return l.Dial() },
@@ -83,7 +118,7 @@ func Handle(handler fasthttp.RequestHandler) func(event events.APIGatewayProxyRe
 			header[string(k)] = string(v)
 		})
 
-		var output = events.APIGatewayProxyResponse{
+		var output = Response{
 			IsBase64Encoded: true,
 			StatusCode:      resp.StatusCode(),
 			Body:            base64.RawStdEncoding.EncodeToString(resp.Body()),
